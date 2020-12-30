ad60eb6dcd0df4da83e47a9450eb44f73ba8323d
buai.cr <2732914121@qq.com>
2020-12-31 00:37:09 +0800

    支持html的图片优化

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.scss";
 import Test from "./components/Test";
+import Img1 from './static/test.png'; // 图片的引用都是采用这种方式
+
 function App(props) {
   return (
     <div className="App">
       这是首页
       <span className="test">这是测试字体</span>
+      <img src={Img1} />
       <div>
         <Test />
       </div>
+      <div className="backName"></div>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJs = require("uglifyjs-webpack-plugin");
 
 module.exports = {
     // devtool: 'inline-source-map',
@@ -11,6 +13,16 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist"),
+    },
+    optimization: {
+        // 优化项
+        minimizer: [
+            new UglifyJs({
+                cache: true, //是否缓存
+                parallel: true, //是否并发操作
+            }),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -25,17 +37,20 @@ module.exports = {
     devServer: {
         contentBase: "./dist",
         hot: true,
+        compress: true,
         inline: true,
+        progress: true,
         open: true,
     },
     module: {
         rules: [
             {
+                //支持css、less、scss
                 test: /\.(c|le|sc)ss$/,
                 use: [
                     "style-loader",
                     {
-                        loader: MiniCssExtractPlugin.loader,
+                        loader: MiniCssExtractPlugin.loader, // 支持分离css，顺序不能随意变动
                         options: {
                             esModule: false,
                         },
@@ -47,6 +62,22 @@ module.exports = {
                 ],
             },
             {
+                // 加载图片
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 10 * 1024, // 图片小于10KB值的时候变为base64直接附着在js中(这里的单位为B)
+                            outputPath: "static/", //打包后的图片放在 dist/static/下边
+                            name: "[name][hash:8].[ext]",
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+            {
+                // 支持ES6以及reactJSX语法
                 test: /\.jsx?$/,
                 exclude: /node_modules/, // 排除 node_modules 文件夹
                 use: {
